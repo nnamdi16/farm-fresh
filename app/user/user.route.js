e2fa2a1b73f5a4393a4a8843e8c440a1830d6894
Nnamdi <nnwabuokei@paga.com>
2020-08-23 03:25:17 +0100

    feat:Documented the route in user.route.js

--- app/user/user.route.js
+++ app/user/user.route.js
@@ -3,7 +3,14 @@ const router = Router();
 const UserController = require('./user.controller');
 const {registerUser,authenticateUser} = UserController;
 
+/**
+ * @path {POST} /apiv1/register
+ */
 router.route('/register').post(registerUser);
+
+/**
+ * @path {POST} /apiv1/login
+ */
 router.route('/login').post(authenticateUser);
 
 module.exports = router;
