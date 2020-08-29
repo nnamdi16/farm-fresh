5a3f7021efadb3ee6e321f4427463b5578515b3f
Nnamdi <nnwabuokei@paga.com>
2020-08-29 21:03:35 +0100

    chore:refactored the registerUser controller

--- app/user/user.controller.js
+++ app/user/user.controller.js
@@ -29,31 +29,32 @@ exports.registerUser = async(req,res) => {
         res.set("Content-Type", "application/json");
         res.set("Accept", "application/json");
         const requestParameters = req.body;
+       
         const userDetails = await registerUser(requestParameters);
-        if (userDetails.error) {
-            return res.status(200).json(
-                {
-                    success: false,
-                    message: userDetails.message
-                }
-            )
-            
-        }
         const {
             error,
             message
         } = userDetails;
+        if (error) {
+            return res.status(200).json(
+                {
+                    success: error,
+                    message
+                }
+            )
+            
+        }
+    
         res.status(200).send({
             error,
             message
         });
 
-
     } catch (error) {
         res.status(500).json(
             {
-                success: false,
-                message: error.message
+                success: error,
+                message
             }
         )
     }
