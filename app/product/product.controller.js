f29f12777b698ef7791f9714030d61feb7f6c139
Nnamdi <nnwabuokei@paga.com>
2020-08-29 20:59:52 +0100

    feat:created createproduct and updateProduct controller function

---
+++ app/product/product.controller.js
@@ -0,0 +1,73 @@
+const {createProduct,updateProduct} = require('./product.service');
+
+
+exports.createProduct = async(req,res) => {
+    try {
+        res.set("Content-Type", "application/json");
+        res.set("Accept", "application/json");
+        const requestParameters = req.body;
+        
+        const productDetails = await createProduct(requestParameters);
+        const {
+            error,
+            message
+        } = productDetails;
+        if (error) {
+            return res.status(200).json(
+                {
+                    success: error,
+                    message
+                }
+            )
+            
+        }
+    
+        res.status(200).send({
+            error,
+            message
+        });
+    } catch (error) {
+        res.status(500).json(
+            {
+                success: error,
+                message
+            }
+        )
+    }
+}
+
+
+exports.updateProduct = async(req,res) => {
+    try {
+        res.set("Content-Type", "application/json");
+        res.set("Accept", "application/json");
+        const requestParameters = req.body;
+        
+        const productUpdate = await updateProduct(requestParameters);
+        const {
+            error,
+            message
+        } = productUpdate;
+        if (error) {
+            return res.status(200).json(
+                {
+                    success: error,
+                    message
+                }
+            )
+            
+        }
+    
+        res.status(200).send({
+            error,
+            message
+        });
+    } catch (error) {
+        res.status(500).json(
+            {
+                success: error,
+                message
+            }
+        )
+    }
+}
