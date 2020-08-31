856b57b5c3be618f38a8d047c7ab59220b7f82dd
Nnamdi <nnwabuokei@paga.com>
2020-08-31 01:03:01 +0100

    feat: added middleware authenticateToken to create product endpoint.

--- app/product/product.route.js
+++ app/product/product.route.js
@@ -2,8 +2,9 @@ const {Router} = require('express');
 const router = Router();
 const ProductController = require('./product.controller');
 const {createProduct,updateProduct} = ProductController;
+const {authenticateToken} = require('../util/auth')
 
-router.route('/create').post(createProduct);
+router.route('/create').post(authenticateToken,createProduct);
 router.route('/update').post(updateProduct);
 
 module.exports = router;
