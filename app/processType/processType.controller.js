fe100d750dbf36c45e19e5f5e4caa90aec58650a
Nnamdi <nnwabuokei@paga.com>
2020-08-31 07:42:35 +0100

    chore: added jsdoc comments for documentation

--- app/processType/processType.controller.js
+++ app/processType/processType.controller.js
@@ -1,5 +1,26 @@
 const {createProcessType} = require('./processType.service');
 
+/**
+ *This function creates a new Process type
+ * @name createProcessType
+ * 
+ * @path {POST} /processType/create
+ * 
+ * 
+ * @function
+ * 
+ * @param {Object}  req                 request parameters
+ * @param {String}  req.processTypeId   Process Type name
+ * @param {String}  req.description     Description of the process type
+ * 
+ * @param {Object}  res                 response parameters
+ * @param {Boolean}  res.success         The state of the response, either true or false
+ * @param {String}  res.message         A short message giving more information about the response.
+ * 
+ * @chain {@link https://kisankranti.herokuapp.com/}
+ * 
+ * 
+ */
 exports.createProcessType = async(req,res) => {
     try {
         res.set("Content-Type", "application/json");
