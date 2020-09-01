a0a73e79310af9d4909d684d52b914393cce35b5
Nnamdi <nnwabuokei@paga.com>
2020-09-01 04:18:09 +0100

    Added the getSellerProducts route

--- app/product/product.route.js
+++ app/product/product.route.js
@@ -1,10 +1,11 @@
 const {Router} = require('express');
 const router = Router();
 const ProductController = require('./product.controller');
-const {createProduct,updateProduct} = ProductController;
-const {authenticateToken} = require('../util/auth')
+const {createProduct,updateProduct,getSellerProducts} = ProductController;
+const {authenticateToken} = require('../util/auth');
 
 router.route('/create').post(authenticateToken,createProduct);
 router.route('/update').post(updateProduct);
+router.route('/:sellerId').get(getSellerProducts);
 
 module.exports = router;
