03034f0cfcc0f625c4873f027b0c1410eb1eafc0
Nnamdi <nnwabuokei@paga.com>
2020-09-05 04:31:53 +0100

    feat:create a route for creating process

---
+++ app/process/process.route.js
@@ -0,0 +1,9 @@
+const {Router} = require('express');
+const router = Router();
+const ProcessController = require('./process.controller');
+const {createProcess} = ProcessController;
+
+/**
+ * @path {POST} /process/create
+ */
+router.route('/create').post(createProcess);
