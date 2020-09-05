2fc0c230d6a6218bfc70210b57810a2cf77500ab
Nnamdi <nnwabuokei@paga.com>
2020-09-05 04:34:00 +0100

    feat: added a complete registration method in user.controller file

--- app/user/user.controller.js
+++ app/user/user.controller.js
@@ -1,4 +1,4 @@
-const {registerUser,authenticateUser} = require("./user.service");
+const {registerUser,authenticateUser,completeRegistration} = require("./user.service");
 
 
 /**
@@ -61,6 +61,62 @@ exports.registerUser = async(req,res) => {
     }
 }
 
+
+/**
+ *This function creates a new user.
+ * @name registerUser
+ * 
+ * @path {POST} /apiv1/register
+ * 
+ * 
+ * @function
+ * 
+ * @param {Object}  req             request parameters
+ * @param {String}  req.firstName   Firstname of the User
+ * @param {String}  req.lastName    Lastname of the User
+ * @param {String}  req.phoneNumber Phone number of the User
+ * @param {String}  req.password    Password of the User
+ * 
+ * @param {Object}  res             response parameters
+ * @param {Boolean}  res.success     The state of the response, either true or false
+ * @param {String}  res.message     A short message giving more information about the response.
+ * 
+ * @chain {@link https://kisankranti.herokuapp.com/}
+ * 
+ * 
+ */
+exports.completeRegistration = async(req,res) => {
+    try {
+        res.set("Content-Type", "application/json");
+        res.set("Accept", "application/json");
+        const requestParameters = req.body;
+       
+        const userCompleteDetails = await completeRegistration(requestParameters);
+        const {error,message} = userCompleteDetails;
+
+        if (error) {
+            return res.status(200).json(
+                {
+                    success:!error,
+                    message
+                }
+            )
+        }
+        
+        res.status(200).send({
+            success:true,
+            data:userCompleteDetails
+        });
+
+    } catch (error) {
+        res.status(500).json(
+            {
+                success: false,
+                message:error.message
+            }
+        )
+    }
+}
 
 /**
  *This function authenticate and allows auser to be logged in .
