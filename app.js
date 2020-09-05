e4251b70228588adefd0c6521204cdb8d325e1cb
Nnamdi <nnwabuokei@paga.com>
2020-09-05 04:29:16 +0100

    chore:set useFindAndModify on mongoose.connect to be false

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var cors = require("cors");
 var MONGODB_URL = process.env.MONGODB_URL;
 console.log(MONGODB_URL);
 var mongoose = require("mongoose");
-mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify:false }).then(() => {
 	//don't show the log when it is test
 	if(process.env.NODE_ENV !== "test") {
 		console.log("Connected to %s", MONGODB_URL);
