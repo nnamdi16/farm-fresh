ce2b78e14ebb112a44f3d6f8144119898d2474f1
Nnamdi <nnwabuokei@paga.com>
2020-09-05 04:35:13 +0100

    chore:created a route to complete registration

--- app/user/user.route.js
+++ app/user/user.route.js
@@ -1,12 +1,17 @@
 const {Router} = require('express');
 const router = Router();
 const UserController = require('./user.controller');
-const {registerUser,authenticateUser,logoutUser} = UserController;
+const {registerUser,authenticateUser,logoutUser,completeRegistration} = UserController;
 
 /**
  * @path {POST} /apiv1/register
  */
 router.route('/register').post(registerUser);
+
+/**
+ * @path {POST} /apiv1//complete-registration
+ */
+router.route('/complete-registration').post(completeRegistration);
 
 /**
  * @path {POST} /apiv1/login
