e3317fbc8558f1ad8100da93eea6efebdd662716
Nnamdi <nnwabuokei@paga.com>
2020-09-05 04:31:10 +0100

    feat: created a model for the process resource

---
+++ app/process/process.model.js
@@ -0,0 +1,49 @@
+/**
+ * Module dependencies
+ */
+
+ const {Schema, model} = require('mongoose');
+
+ const processStatus = Object.freeze(
+     {
+         Pending:'PENDING',
+         Expired:'EXPIRED',
+         Success:'SUCCESS'
+     }
+ )
+ 
+ const ProcessSchema = new Schema(
+     {
+         processTypeId: {
+             type:Schema.Types.ObjectId,
+             ref:"ProcessType"
+         },
+         createdBy: {
+             type:Schema.Types.ObjectId,
+             ref:"User"
+         },
+         updatedBy: {
+             type:Schema.Types.ObjectId,
+             ref:"User"
+         },
+         processCode: {
+             type:String
+         },
+         processStatus: {
+             type:String,
+             enum:Object.values(processStatus),
+             default:processStatus.Pending
+         },
+         token: {
+             type: String
+         }
+     },
+
+     {timestamps:true}
+ )
+
+ Object.assign(ProcessSchema.statics, {processStatus});
+
+ model("Process", ProcessSchema);
+
+ module.exports = model("Process");
