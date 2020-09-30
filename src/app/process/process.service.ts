475f4aac62f4685815aa6a70661788b9c392a86c
Nnamdi <nnwabuokei@paga.com>
2020-09-30 23:45:43 +0100

    Added the necessary typings for process.service.ts

--- src/app/process/process.service.ts
+++ src/app/process/process.service.ts
@@ -1,6 +1,9 @@
+import { Schema} from "mongoose";
+import { IProcess } from "./process.interface";
+
 const ProcessSchema = require('./process.model');
 
-exports.createProcess = async function (data) {
+exports.createProcess = async function (data:IProcess) {
     try {
         const {processTypeId, createdBy, updatedBy, processCode, processStatus, token } = data;
         const createProcessObject= new ProcessSchema(
